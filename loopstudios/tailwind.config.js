7247b49350af6a4c5bd321df06e2062837f31ce9
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-10-14 12:01:55 +0200

    Loopstudios project initial setup

---
+++ loopstudios/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+    },
+  },
+  plugins: [],
+};
